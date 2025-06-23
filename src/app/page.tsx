2c5bf991df3fe94064642dc8991c583c5a66fdda
Reossann <s1f102502283@iniad.org>
2025-06-23 15:32:00 +0900

    かぶらないようにした（アバターー）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,20 +55,34 @@ export default function Home() {
     for (let y = 0; y < 9; y++) {
       for (let x = 0; x < 9; x++) {
         if (anotherbaord[y][x] === 1) {
-          if (Counter >= 4) {
-            const ram = Math.floor(Math.random() * (4 - 1) + 1);
+          let Notrapped = 0;
+          do {
+            if (Counter >= 4) {
+              const ram = Math.floor(Math.random() * (4 - 1) + 1);
 
-            const dy_dx = direction[ram];
-            console.log(dy_dx);
-            newbaord[y + dy_dx[0]][x + dy_dx[1]] = 1;
-          } else {
-            const ram = Math.floor(Math.random() * 4);
-            const dy_dx = direction[ram];
-            console.log(10);
-            console.log(dy_dx);
-            newbaord[y + dy_dx[0]][x + dy_dx[1]] = 1;
-            Counter += 1;
-          }
+              const dy_dx = direction[ram];
+              console.log(dy_dx);
+              if (newbaord[y + dy_dx[0]][x + dy_dx[1]] === 1) {
+                console.log(999);
+                continue;
+              }
+              newbaord[y + dy_dx[0]][x + dy_dx[1]] = 1;
+
+              Notrapped += 1;
+            } else {
+              const ram = Math.floor(Math.random() * 4);
+              const dy_dx = direction[ram];
+              console.log(10);
+              console.log(dy_dx);
+              if (newbaord[y + dy_dx[0]][x + dy_dx[1]] === 1) {
+                console.log(999);
+                continue;
+              }
+              newbaord[y + dy_dx[0]][x + dy_dx[1]] = 1;
+              Notrapped += 1;
+              Counter += 1;
+            }
+          } while (Notrapped === 0);
         }
       }
     }
